4202ba4f7bfd7d46d0f990837d3d0007123fedd1
Marton Sereg <msereg@hortonworks.com>
2016-08-18 16:29:27 +0200

    save version for other rc jobs

--- scripts/build-rc.sh
+++ scripts/build-rc.sh
@@ -5,3 +5,5 @@
 VERSION=$(git describe --abbrev=0 --tags | cut -d '-' -f 1 | cut -d '.' -f 1,2)
 git checkout -b "rc-$VERSION"
 git push -u origin "rc-$VERSION"
+
+echo "VERSION=${VERSION}" > $WORKSPACE/version
